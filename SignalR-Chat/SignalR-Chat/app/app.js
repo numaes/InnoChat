81e3518869721dee784f5d8ede0dacf6cc4af08f
Dani <daesgar90@outlook.com>
2016-02-04 16:10:13 +0100

    Added styles

--- SignalR-Chat/SignalR-Chat/app/app.js
+++ SignalR-Chat/SignalR-Chat/app/app.js
@@ -1,38 +1,31 @@
 ﻿$(function () {
     var chat = $.connection.chatHub;
+    var username;
+    do {
+        username = prompt("Insert your username: ");
+    } while (username == null || username == "");
 
     chat.client.updateUsers = function (userCount, userList) {
-        $('#onlineUsersCount').text('Users online: ' + userCount);
+        $('#onlineUsersCount').text('Online users: ' + userCount);
         $('#userList').empty();
         userList.forEach(function (username) {
-            $('#userList').append('<tr id="' + username + '"><td>' + username + '</td></tr>');
+            $('#userList').append('<li>' + username + '</li>');
         });
     }
 
     chat.client.broadcastMessage = function (username, message) {
-        $('#messagesArea').append('<li>' + username + ': ' + message);
+        $('#messagesArea').append('<li><strong>' + username + '</strong>: ' + message);
     }
 
     $.connection.hub.start().done(function () {
-        var username = prompt("Insert your username: ");
         chat.server.connect(username);
     });
 
     $('#btnSendMessage').click(function(){
         var message = $('#userMessage').val();
         chat.server.send(message);
+        $('#userMessage').val("");
     });
-
-    var call = function () {
-        $.ajax({
-            type: "POST",
-            url: "http://legoev3api.azurewebsites.net:80/api/Values",
-            data: 1,
-            
-       });
-    }
-
-    setInterval(call, 500);
 
 });
 
